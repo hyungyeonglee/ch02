cf8a637024bba0dc62747bab560f553d3b97b043
LeeHyunGyeong <kkyungzz1@gmail.com>
2020-04-28 18:07:20 +0900

    4/28 수업

---
+++ src/C_DoubleEx.java
@@ -0,0 +1,15 @@
+public class C_DoubleEx
+{
+
+	public static void main(String[] args)
+	{
+		double dnum=1;
+		
+		for(int i=0; i<10000; i++)
+		{
+			dnum=dnum+0.1;
+			// 문자 
+		}
+		System.out.println(dnum);
+	}
+}
